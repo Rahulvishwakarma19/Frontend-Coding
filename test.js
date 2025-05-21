9621e6f8b6712c5a2b5fb785a9111e74bd0acdae
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2025-05-21 15:49:20 +0530

    variable

---
+++ test.js
@@ -0,0 +1,9 @@
+const accountId = 123456;
+let accountEmail = "rahul@gmail.com";
+var accountPassword = 421;
+accountCity = "Mumbai";
+
+
+console.log("Rahul")
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
