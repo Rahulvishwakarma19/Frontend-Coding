39c9e30fb76139dfaa971b9176bbfe15a39db682
Rahul Vishwakarma <rahulvishwakarma8171@gmail.com>
2025-05-22 15:47:50 +0530

    datatype_conversion

---
+++ datatype.js
@@ -0,0 +1,23 @@
+// Integer to String Conversion.
+let num = 55;
+console.log(typeof num);
+console.log(num)
+let stringNum = String(num);
+console.log(typeof stringNum);
+console.log(stringNum)
+
+//  String to Integer  Conversion.
+let sNum = "44";
+console.log(typeof sNum);
+console.log(sNum)
+let sNumber = Number(sNum);
+console.log(typeof sNumber);
+console.log(sNumber)
+
+// Conversion in boolean
+let isLoggedIn = 1
+console.log(typeof isLoggedIn)
+console.log(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
